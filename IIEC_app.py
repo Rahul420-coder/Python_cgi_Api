0707df649ddf7d9158328a901cb592f6d83002af
Rahul Rathod <63442215+Rahul420-coder@users.noreply.github.com>
2020-09-29 01:59:20 -0700

    Update IIEC_app.py

--- IIEC_app.py
+++ IIEC_app.py
@@ -1,47 +1,83 @@
-import pyttsx3
-import os
+#!/usr/bin/python3 
 
-pyttsx3.speak("hey... Rahul Rathod. i'm Your System. and how are you? I think your not fine. ok. Tell me. ")
 
-while True:
+import cgi
+import subprocess as sb
 
-	pyttsx3.speak(" What i do for you ")
-	print("hey... What i do for you :)  " , end="")
-	text = input()
+print("content-type: text/html")
+print()
 
-	if (("open" in text) or ("run" in text)) and (("Chrome" in text) or ("chrome" in text)):
-		pyttsx3.speak("Chrome Browser is Successfully open.")
-		os.system("chrome")
-		pyttsx3.speak("Chrome Browser is Closed.")
-		print("\n===========================================================================================\n\n\n                        Chrome Browser was Successfully opened.                           \n\n\n===========================================================================================\n" )
-		
-	elif (("run" in  text) or ("open" in text)) and (("notepad" in text) or ("texteditor" in text)):
-		pyttsx3.speak("Notepad TextEditor is Successfully open.")
-		os.system("notepad")
-		pyttsx3.speak("Notepad TextEditor is Closed.")
-		print("\n===========================================================================================\n\n\n                      Notepad TextEditor was Successfully opened.                           \n\n===========================================================================================\n" )
-	
-	elif (("open" in text) or ("run" in text)) and (("vlc" in text) or ("video" in text)):
-		pyttsx3.speak("VLC Media Player is Successfully open.")
-		os.system("vlc")
-		pyttsx3.speak("VLC Media Player is Closed.")
-		print("\n===========================================================================================\n\n\n                      VLC Video Player was Successfully opened.                           \n\n\n===========================================================================================\n" )
 
-	elif (("open" in text) or ("run" in text)) and (("control" in text) or ("panel" in text) or ("controlpanel" in text)):
-		pyttsx3.speak("Control Panel is Successfully open.")
-		os.system("control")
-		pyttsx3.speak("Control Panel is Closed.")
-		print("\n===========================================================================================\n\n\n                        Contrl Panel was Successfully opened.                           \n\n\n===========================================================================================\n" )
-	
-	elif ("python" in text) and (("-v" in text) or ("version" in text)):
-		pyttsx3.speak("Python Version is Python 3.8.5.")
-		print("\n===========================================================================================\n\n\n                      Python Version is Python 3.8.5.                           \n\n\n===========================================================================================\n" )
+a = cgi.FieldStorage()
+cmd = a.getvalue("x")
 
-	elif (("exit" in text) or ("quit" in text)):
-		pyttsx3.speak("Have a Nice day Sir . Now i think Your fine :)")
-		print("\n===========================================================================================\n\n\n                          Have a nice day Sir.. okey Bye :)                            \n\n\n===========================================================================================\n" )
-		break
 
-	else:
-		pyttsx3.speak("Your Enter Wrong text. so please Enter valid text.")
-		print("\n===========================================================================================\n\n\n                            Your Enter Wrong Text :(                            \n\n\n===========================================================================================\n")
+if (("run" in cmd) or ("what" in cmd) or ("today's" in cmd) or ("execute" in cmd))and (("date" in cmd)):
+    x = sb.getoutput("date")
+    print(x)
+    sb.getoutput("espeak-ng `date`")
+elif (("month" in cmd) or ("what" in cmd) or ("execute" in cmd)or ("today's" in cmd))and (("cal" in cmd) or ("calendar" in cmd)):
+    x = sb.getoutput(" cal")
+    print(x)
+    sb.getoutput(" espeak-ng `cal`")
+elif (("hadoop" in cmd))and (("version" in cmd)):
+    x = sb.getoutput("hadoop version")
+    print(x)
+    sb.getoutput(" espeak-ng 'hadoop version'")
+elif (("java" in cmd))and (("version" in cmd)):
+    x = sb.getoutput(" java -version")
+    print(x)
+    sb.getoutput(" espeak-ng 'java version'")
+elif (("docker" in cmd))and (("version" in cmd)):
+    x = sb.getoutput(" docker version")
+    print(x)
+    sb.getoutput(" espeak-ng 'docker version'")
+elif (("python" in cmd))and (("version" in cmd)):
+    x = sb.getoutput("python3 -V")
+    print(x)
+    sb.getoutput(" espeak-ng '`python3 -V`'")
+elif (("open" in cmd))and (("firefox" in cmd)):
+    x = sb.getoutput(" espeak-ng 'sorry not supported now'")
+    print(x)
+elif (("launch" in cmd))and (("docker" in cmd) or ("container" in cmd)):
+    x = sb.getoutput("docker run -dit --name os ubuntu ")
+    print(x)
+    sb.getoutput(" espeak-ng 'container launched'")
+elif (("show" in cmd) or ("running" in cmd))and (("docker" in cmd) or ("container" in cmd)):
+    x = sb.getoutput(" docker ps")
+    print(x)
+    sb.getoutput(" espeak-ng 'running container'")
+elif (("remove" in cmd) or ("delete" in cmd))and (("docker" in cmd) or ("container" in cmd)):
+    x = sb.getoutput(" docker rm -f os")
+    print(x)
+    sb.getoutput(" espeak-ng 'container removed'")
+elif (("initialize" in cmd) or ("install" in cmd))and (("terraform" in cmd) or ("plugins" in cmd)):
+    x = sb.getoutput(" terraform init")
+    print(x)
+    sb.getoutput(" espeak-ng 'plugin installed'")
+elif (("working" in cmd) or ("track" in cmd))and (("terraform" in cmd) or ("plan" in cmd)):
+    x = sb.getoutput(" terraform plan")
+    print(x)
+    sb.getoutput(" espeak-ng 'good'")
+elif (("validate" in cmd) or ("correct" in cmd))and (("terraform" in cmd) or ("code" in cmd)):
+    x = sb.getoutput("terraform validate")
+    print(x)
+    sb.getoutput(" espeak-ng 'successful'")
+elif (("run" in cmd) or ("apply" in cmd))and (("terraform" in cmd) or ("code" in cmd)):
+    x = sb.getoutput(" terraform apply --auto-approve")
+    print(x)
+    sb.getoutput(" espeak-ng 'setup done'")
+elif (("delete" in cmd) or ("destroy" in cmd))and (("terraform" in cmd) or ("setup" in cmd)):
+    x = sb.getoutput(" terraform destroy --auto-approve")
+    print(x)
+    sb.getoutput(" espeak-ng 'setup removed'")
+elif (("check" in cmd) or ("send" in cmd))and (("connectivity" in cmd) or ("packets" in cmd) or ("google" in cmd)):
+    x = sb.getoutput(" ping -c 4 8.8.8.8")
+    print(x)
+    sb.getoutput(" espeak-ng 'yes it is working good'")
+elif ("exit" in cmd) or ("quit" in cmd):
+	x = sb.getoutput("espeak-ng 'Have a nice day sir...'")
+	print("Have a nice dir sir... :)")
+else:
+    sb.getoutput(" espeak-ng 'your enter wrong text.'")
+    print("Your Enter Wrong Text, so please enter valid text !!")
